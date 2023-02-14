83ec217968f2e13fd3a19e57ccb3777dbbd63c8c
yeonblue <yeondublue@naver.com>
2023-02-14 09:34:08 +0900

    Initial Commit

---
+++ SwiftUIPortfolio/SwiftUIPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPortfolioApp.swift
+//  SwiftUIPortfolio
+//
+//  Created by yeonBlue on 2023/02/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
