561440cc4a6a88294adb56e4f672ef8a34c044d7
yeonblue <yeondublue@naver.com>
2023-03-16 14:42:25 +0900

    add ContentView toolbar

--- SwiftUIPortfolio/DataController.swift
+++ SwiftUIPortfolio/DataController.swift
@@ -8,12 +8,29 @@
 import Foundation
 import CoreData
 
+enum SortType: String {
+    case dateCreated = "creationDate"
+    case dateModified = "modificationDate"
+}
+
+enum Status {
+    case all
+    case open
+    case closed
+}
+
 class DataController: ObservableObject {
     
     @Published var selectedFilter: Filter? = .all
     @Published var selectdIssue: Issue?
     @Published var filterText = ""
     @Published var filterTokens = [Tag]()
+    
+    @Published var filterEnabled = false
+    @Published var filterPriority = -1
+    @Published var filterStatus = Status.all
+    @Published var sortType = SortType.dateCreated
+    @Published var sortNewestFirst = true
     
     static var preview: DataController = {
         let dataController = DataController(inMemory: true)
@@ -186,8 +203,22 @@ class DataController: ObservableObject {
             }
         }
         
+        if filterEnabled {
+            if filterPriority >= 0 {
+                let priorityFilter = NSPredicate(format: "priority = %d", filterPriority)
+                predicates.append(priorityFilter)
+            }
+            
+            if filterStatus != .all {
+                let lookForClosed = filterStatus == .closed
+                let statusFilter = NSPredicate(format: "completed = %@", NSNumber(value: lookForClosed))
+                predicates.append(statusFilter)
+            }
+        }
+        
         let request = Issue.fetchRequest()
         request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
+        request.sortDescriptors = [NSSortDescriptor(key: sortType.rawValue, ascending: sortNewestFirst)]
         
         let allIssues = (try? container.viewContext.fetch(request)) ?? []
         return allIssues.sorted()
